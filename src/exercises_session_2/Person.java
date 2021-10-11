2827fab75ce66abe8dd61d4bcb73c09b32feb662
meesvdent <meesvdent@gmail.com>
2021-10-11 09:23:13 +0200

    adapted Person class: added marry to other person instance

--- src/exercises_session_2/Person.java
+++ src/exercises_session_2/Person.java
@@ -5,26 +5,25 @@ public class Person {
     // declare variables
     String name;
     int age;
-    boolean isMarried;
+    Person spouse;
 
-    public Person(String personName, int personAge, boolean personIsMarried){
+    public Person(String personName, int personAge){
         name = personName;
         age = personAge;
-        isMarried = personIsMarried;
+        spouse = null;
     }
 
     // takes two Person instances and sets their isMarried value to true.
-    public void marry(Person personOne, Person personTwo){
-        personOne.isMarried = true;
-        personTwo.isMarried = true;
+    public void marry(Person self, Person other){
+        self.spouse = other;
+        other.spouse = self;
     }
 
     public static void main(String[] args){
-        Person firstPerson = new Person("name1", 58, false);
-        Person secondPerson = new Person("name2", 24, false);
+        Person firstPerson = new Person("name1", 58);
+        Person secondPerson = new Person("name2", 24);
 
-        System.out.println("Married: " + firstPerson.isMarried + " " + secondPerson.isMarried);
         firstPerson.marry(firstPerson, secondPerson);
-        System.out.println("Married: " + firstPerson.isMarried + " " + secondPerson.isMarried);
+        System.out.println("Married: " + firstPerson.spouse.name + " " + secondPerson.spouse.name);
     }
 }
