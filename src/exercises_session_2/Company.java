788f14ff961cd4ccf5b6e54451293514b59edcb9
meesvdent <meesvdent@gmail.com>
2021-10-11 09:06:24 +0200

    Added company class

---
+++ src/exercises_session_2/Company.java
@@ -0,0 +1,27 @@
+package exercises_session_2;
+
+public class Company {
+
+    // variable initialization
+    String companyName, location;
+    static String compareLocation = "Belgium";
+
+    public Company(String companyCompanyName, String companyLocation){
+        // constructor method
+        companyName = companyCompanyName;
+        location = companyLocation;
+    }
+
+    // function which returns whether location is equal to belgium
+    boolean isBelgian(){
+        return location.equals(compareLocation);
+    }
+
+    public static void main(String[] args){
+        Company microsoft = new Company("Microsoft", "US");
+        Company jansen = new Company("Jansen", "Belgium");
+
+        System.out.println(microsoft.isBelgian());
+        System.out.println(jansen.isBelgian());
+    }
+}
