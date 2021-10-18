91cf116c0b277b025ac329bea7b3faa5ddf13bd0
meesvdent <meesvdent@gmail.com>
2021-10-18 08:26:38 +0200

    Added InterestChecker class

---
+++ src/exercises_session_3/InterestChecker.java
@@ -0,0 +1,23 @@
+package exercises_session_3;
+
+class InterestChecker{
+
+    private static final double baseInterest = 0.1;
+    private static final double fidelityBonus = 0.1;
+    private static final int minNbYearsForFidelityBonus = 3;
+    private static final double passwordBonus = 0.2;
+    private static final String superSecretPassword = "MOREMONEYPLEASE";
+
+
+    public static double calculateInterest(double accountBalance, int nbYearsCustomer, String bonusCode){
+        double totalInterest = baseInterest;
+        if(nbYearsCustomer >= minNbYearsForFidelityBonus){
+            totalInterest += fidelityBonus;
+        }
+        if(bonusCode.equals(superSecretPassword)){
+            totalInterest += passwordBonus;
+        }
+
+        return accountBalance*totalInterest;
+    }
+}
