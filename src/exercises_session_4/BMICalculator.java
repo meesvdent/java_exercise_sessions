88399a28265eb2795238e4922187799deb5d92d3
meesvdent <meesvdent@gmail.com>
2021-10-25 10:58:21 +0200

    Non stopping code

--- src/exercises_session_4/BMICalculator.java
+++ src/exercises_session_4/BMICalculator.java
@@ -6,35 +6,49 @@ import java.util.Locale;
 
 public class BMICalculator {
     // Main method
-    public static void main(String[] args){
+    public static void main(String[] args) {
         // declare variables
+        double weight, height, BMI;
+        boolean BMIUnassigned;
         // set variables
-        try{
+        BMIUnassigned = true;
+
+        while(BMIUnassigned){
             System.out.println("Enter person weight: ");
-            double weight = inputDouble();
+            weight = inputDouble();
             System.out.println("Enter person height: ");
-            double height = inputDouble();
+            height = inputDouble();
             // calculate BMI
-            double BMI = calculateBMI(weight, height);
+            BMI = calculateBMI(weight, height);
+            BMIUnassigned = false;
             // print to screen
             System.out.println("Your BMI is " + BMI + ".");
-        } catch(InputMismatchException im){
-            System.out.println("Arguments for weight and height should be double!");
-            main(null);
         }
     }
 
     // method calculating BMI
-    public static double calculateBMI(double weight,double height){
-        double BMI;
-        BMI = weight/(height*height);
-        return BMI;
+    public static double calculateBMI(double weight,double height)throws ArithmeticException{
+        if(weight <= 0 | weight > 1000){
+            throw new ArithmeticException();
+        } else if(height <= 0 | height > 4){
+            throw new ArithmeticException();
+        } else{
+            double BMI;
+            BMI = weight/(height*height);
+            return BMI;
+        }
     }
 
-    public static double inputDouble(){
+    public static double inputDouble() throws InputMismatchException{
         double input_double;
-        Scanner scan = new Scanner(System.in).useLocale(Locale.US);
-        input_double = scan.nextDouble();
-        return input_double;
+        try{
+            Scanner scan = new Scanner(System.in).useLocale(Locale.US);
+            input_double = scan.nextDouble();
+            return input_double;
+        } catch(InputMismatchException im){
+            System.out.println("Arguments for weight and height should be double!");
+            input_double = inputDouble();
+            return input_double;
+        }
     }
 }
