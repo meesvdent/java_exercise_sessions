ebe5f2b0861374bd20568dfa8e5500dba45a2a80
meesvdent <meesvdent@gmail.com>
2021-10-25 08:50:11 +0200

    Exercise 3

---
+++ src/exercises_session_4/Frozen.java
@@ -0,0 +1,21 @@
+package exercises_session_4;
+
+public class Frozen {
+
+    public static void main (String [] args){
+
+    int [] clientid = {1,2,3,4,5};
+    String [] clientName = {"Anna", "Elsa", "Olaf", "Hans", "Kristoff"};
+    double [] accountBalance = {1000, 5000, 9, 55, 23};
+
+    for(int i = 1; i<=5; i++) {
+        try{
+            System.out.println(clientName[i]+" has "+accountBalance[i]+" dollars. \n");
+        } catch(Exception exc){
+            System.out.println("index out of bounds");
+            break;
+        }
+    }
+}
+
+}
